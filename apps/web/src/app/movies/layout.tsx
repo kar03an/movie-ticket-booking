939f43de7538991457e6f5e0460eacfde887b543
The-Phoenics <kar9205an@gmail.com>
2026-07-27 15:07:15 +0530

    fix: remove unnecessary rendered ;

--- apps/web/src/app/movies/layout.tsx
+++ apps/web/src/app/movies/layout.tsx
@@ -2,5 +2,5 @@ import HeaderBar from "@/components/header-bar";
 import type { ReactNode } from "react";
 
 export default function MoviesLayout({ children }: { children: ReactNode }) {
-  return <HeaderBar>{children};</HeaderBar>;
+  return <HeaderBar>{children}</HeaderBar>;
 }
